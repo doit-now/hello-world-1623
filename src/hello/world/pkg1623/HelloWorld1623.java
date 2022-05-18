98fe9410ce254b857b3ab4cdf4b0e71bf8e0ea0a
doit-now <hoang.nguyenthe@gmail.com>
2022-05-18 13:53:38 +0700

    1st lam chuyen ay - upload code to github

---
+++ src/hello/world/pkg1623/HelloWorld1623.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello.world.pkg1623;
+
+/**
+ *
+ * @author giao.lang
+ */
+public class HelloWorld1623 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to GitHub & Java World");
+    }
+    
+}
